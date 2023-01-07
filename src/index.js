00fb6e8dea76deaea83a8d91a262e8ffe9cc9be3
Spajkowiecki <pcio10@wp.pl>
2023-01-07 04:22:55 +0100

    Fixing issues

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './css/styles.css';
-import debounce from 'lodash';
+import { debounce } from 'lodash';
 import { fetchCountries } from './fetchCountries';
 import Notiflix from 'notiflix';
 
